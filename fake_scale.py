6350a1864c18f9cc57260b4631c24cfe718fb560
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2025-05-15 23:51:01 -0300

    Make fake_scale compatible with current scale

--- fake_scale.py
+++ fake_scale.py
@@ -6,10 +6,14 @@ class Scale:
         self.weight = 0
         self.counter = 0
 
-    def zero_and_start(self):
+    def set_zero(self, _=None):
         self.weight = 0
 
-    def read_weight(self, target):
+    def set_scale(self, _=None):
+        self.zero()
+        self.scale_factor = 1
+
+    def read_weight(self, target=40):
         if self.counter > 20:
             if self.weight < target + 5:
                 self.weight += (0.7 * random() + 0.3 * self.weight) / 5
