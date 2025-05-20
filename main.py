49ff099a6f9c1d314cbf087ab7d22c3f278623ef
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2025-05-20 11:44:44 -0300

    Wrap main.py in try except block that starts webrepl for debuging LAST_ERROR stores the error

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-
+import sys
+import io
 import gc
 import asyncio
+import webrepl
 from deepsleep import DeepSleepTimer
 from microdot import Microdot
 from network_tools import Wifi
@@ -9,39 +11,48 @@ from storage import Storage
 from routs import coffee_machine, last_time_storage
 
 
-configuration = Storage(filename='config.json')
+LAST_ERROR = None
 
-gc.collect()
+try:
+    configuration = Storage(filename='config.json')
 
-wifi = Wifi(configuration)
-gc.collect()
+    gc.collect()
 
-deepsleeptimer = DeepSleepTimer(configuration,
-                                coffee_machine=coffee_machine)
+    wifi = Wifi(configuration)
+    gc.collect()
+    deepsleeptimer = DeepSleepTimer(configuration,
+                                    coffee_machine=coffee_machine)
 
-app = Microdot()
-
-
-@app.before_request
-def before_request_reset_timer(request):
-    deepsleeptimer.reset()
+    app = Microdot()
 
 
-@app.after_request
-def after_request_reset_timer(request, response):
-    deepsleeptimer.reset()
+    @app.before_request
+    def before_request_reset_timer(request):
+        deepsleeptimer.reset()
 
 
-async def main():
-    button = asyncio.create_task(monitor_button(configuration.get('make_coffee_button'),
-                                 coffee_machine, last_time_storage, deepsleeptimer=deepsleeptimer))
+    @app.after_request
+    def after_request_reset_timer(request, response):
+        deepsleeptimer.reset()
 
-    await wifi.display_credentials_until_connected(display=coffee_machine.display)
 
-    from routs import app as subapp
-    app.mount(subapp)
-    server = app.start_server(port=80, debug=True)
-    deep_sleep_check = asyncio.create_task(deepsleeptimer.check_and_sleep())
-    await asyncio.gather(server, button, deep_sleep_check)
+    async def main_loop():
+        button = asyncio.create_task(monitor_button(configuration.get('make_coffee_button'),
+                                    coffee_machine, last_time_storage, deepsleeptimer=deepsleeptimer))
 
-asyncio.run(main())
+        await wifi.display_credentials_until_connected(display=coffee_machine.display)
+
+        from routs import app as subapp
+        app.mount(subapp)
+        server = app.start_server(port=80, debug=True)
+        deep_sleep_check = asyncio.create_task(deepsleeptimer.check_and_sleep())
+        await asyncio.gather(server, button, deep_sleep_check)
+
+
+    asyncio.run(main_loop())
+except Exception as e:
+    webrepl.start()
+    error_stream = io.StringIO()
+    sys.print_exception(e, error_stream)
+    LAST_ERROR = f"Type: {type(e).__name__}\nMessage: {str(e)}\nTraceback:\n{error_stream.getvalue()}"
+    raise
